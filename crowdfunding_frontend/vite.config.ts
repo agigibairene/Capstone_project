5278d0a23b372a31d74567df38a854ebf34c0f26
agigibairene <i.agigiba@alustudent.com>
2025-06-19 16:41:47 +0200

    deployment

--- crowdfunding_frontend/vite.config.ts
+++ crowdfunding_frontend/vite.config.ts
@@ -14,6 +14,8 @@ export default defineConfig({
       'react': path.resolve(__dirname, 'node_modules/react'),
       'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
     },
+    dedupe: ['react', 'react-dom'], 
+
   },
   server: {
     host: true,
