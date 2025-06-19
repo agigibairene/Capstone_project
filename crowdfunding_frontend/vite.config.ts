c80f7f82fe46837fd5c44efc7afe39af6d0d2b91
agigibairene <i.agigiba@alustudent.com>
2025-06-19 16:09:46 +0200

    deployment

--- crowdfunding_frontend/vite.config.ts
+++ crowdfunding_frontend/vite.config.ts
@@ -1,24 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path' // Make sure to import path
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      jsxRuntime: 'classic'
-    }),
+    react(),
     tailwindcss(),
   ],
-  resolve: {
-    alias: {
-      'react': path.resolve('node_modules/react'),
-      'react-dom': path.resolve('node_modules/react-dom'),
-    }
-  },
   server: {
-    host: '0.0.0.0',
+    host: true,
     port: 8080,
     strictPort: true,
     allowedHosts: [
@@ -26,12 +18,5 @@ export default defineConfig({
       'localhost',
       '.vercel.app'
     ]
-  },
-  build: {
-    // This helps with dependency bundling
-    commonjsOptions: {
-      include: [/node_modules/],
-      transformMixedEsModules: true
-    }
   }
 })
