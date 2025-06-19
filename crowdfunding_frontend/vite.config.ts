449649be4b42ac3a032f1b1bf1b20a2d671a06f3
agigibairene <i.agigiba@alustudent.com>
2025-06-19 19:49:48 +0200

    deployment

--- crowdfunding_frontend/vite.config.ts
+++ crowdfunding_frontend/vite.config.ts
@@ -1,30 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      'react': path.resolve(__dirname, 'node_modules/react'),
-      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    resolve: {
+      alias: {
+        react: path.resolve(__dirname, 'node_modules/react'),
+        'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
+      },
+      dedupe: ['react', 'react-dom'],
     },
-    dedupe: ['react', 'react-dom'], 
-
-  },
-  server: {
-    host: true,
-    port: 8080,
-    strictPort: true,
-    allowedHosts: [
-      'capstone-g7gbz.ondigitalocean.app', 
-      'localhost',
-      '.vercel.app'
-    ]
-  }
-})
+    server: {
+      host: true,
+      port: 8080,
+      strictPort: true,
+      allowedHosts: [
+        'capstone-g7gbz.ondigitalocean.app',
+        'localhost',
+        '.vercel.app',
+      ],
+      ...(mode === 'development' && {
+        hmr: {
+          protocol: 'ws',
+          host: 'localhost',
+        },
+      }),
+    },
+  };
+});
