2f02c6c8d20ce96b64aa92c14b793691df8d0fe6
agigibairene <i.agigiba@alustudent.com>
2025-06-30 13:12:19 +0200

    deployment

--- backend/apis/urls.py
+++ backend/apis/urls.py
@@ -4,15 +4,15 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
-    path('signup/', views.signup_view, name='signup'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('forgot-password/', views.forgot_password_view, name='forgot-password'),
-    path('reset-password/<str:reset_id>/', views.reset_password_view, name='reset-password'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='refresh'),
-    path('token/', TokenObtainPairView.as_view(), name='get_token'),
-    path('userprofile/', views.user_profile_view, name='user-profile'),
-    path('userprofile/update/', views.update_profile_view, name='update-profile'),
+    path('auth/signup/', views.signup_view, name='signup'),
+    path('auth/login/', views.login_view, name='login'),
+    path('auth/logout/', views.logout_view, name='logout'),
+    path('auth/forgot-password/', views.forgot_password_view, name='forgot-password'),
+    path('auth/reset-password/<str:reset_id>/', views.reset_password_view, name='reset-password'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('auth/token/', TokenObtainPairView.as_view(), name='get_token'),
+    path('auth/userprofile/', views.user_profile_view, name='user-profile'),
+    path('auth/userprofile/update/', views.update_profile_view, name='update-profile'),
     
     # KYC PATHS
     path('kyc/investor/submit/', views.submit_investor_kyc, name='submit_investor_kyc'),
