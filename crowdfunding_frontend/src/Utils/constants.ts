951a4fd2d4a32ea8d4bb2968936d07b3d72d8eff
agigibairene <i.agigiba@alustudent.com>
2025-07-02 13:50:18 +0200

    api url

--- crowdfunding_frontend/src/Utils/constants.ts
+++ crowdfunding_frontend/src/Utils/constants.ts
@@ -1,3 +1,3 @@
 export const ACCESS_TOKEN = 'access_token';
 export const RESFRESH_TOKEN = 'refresh_token';
-export const API_URL = 'https://agriconnect-backend-hx28n.ondigitalocean.app/'
+export const API_URL = 'https://agriconnect-backend-hx28n.ondigitalocean.app'
