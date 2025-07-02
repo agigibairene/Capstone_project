bf4b9a146bc9f5c1089c6dfe59656960412770a4
agigibairene <i.agigiba@alustudent.com>
2025-07-02 11:05:39 +0200

    deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -172,6 +172,6 @@ EMAIL_HOST_USER='i.agigiba@alustudent.com'
 EMAIL_HOST_PASSWORD=os.getenv('EMAIL_HOST_PASSWORD')
 
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / 'static']
