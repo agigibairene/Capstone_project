46167cf188c131d8088fa46f1270f7ab37378db6
agigibairene <i.agigiba@alustudent.com>
2025-07-02 09:36:19 +0200

    deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
