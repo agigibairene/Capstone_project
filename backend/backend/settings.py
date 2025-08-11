d0e3920a4f328a51ba22a2bf9fb4543919869d71
agigibairene <i.agigiba@alustudent.com>
2025-08-11 16:42:10 +0200

    secret key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,17 +16,19 @@ from datetime import timedelta
 import os
 from urllib.parse import urlparse
 from celery.schedules import crontab
+from django.core.management.utils import get_random_secret_key
 
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
